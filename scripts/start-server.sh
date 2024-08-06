f5af60ae7846b0ea1654d84dd1557af200ad327f
hyukjunkim1116 <nmkk1234@naver.com>
2024-08-07 04:11:31 +0900

    ecr-code-deploy

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/spring-deploy-test
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar test-built.jar > ./output.log 2>&1 &
+docker stop ecr-test || true
+docker rm ecr-test || true
+docker pull 082186612390.dkr.ecr.ap-northeast-2.amazonaws.com/ecr-test:latest
+docker run -d --name instagram-server -p 8080:8080 082186612390.dkr.ecr.ap-northeast-2.amazonaws.com/ecr-test:latest
 echo "--------------- 서버 배포 끝 -----------------"
