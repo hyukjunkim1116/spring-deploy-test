8ddfcea7a0d7ce7c9c8be0169be8174fa07bc87d
hyukjunkim1116 <nmkk1234@naver.com>
2024-08-07 04:27:00 +0900

    edit start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,5 +4,5 @@ echo "--------------- 서버 배포 시작 -----------------"
 docker stop ecr-test || true
 docker rm ecr-test || true
 docker pull 082186612390.dkr.ecr.ap-northeast-2.amazonaws.com/ecr-test:latest
-docker run -d --name instagram-server -p 8080:8080 082186612390.dkr.ecr.ap-northeast-2.amazonaws.com/ecr-test:latest
+docker run -d --name ecr-test -p 8080:8080 082186612390.dkr.ecr.ap-northeast-2.amazonaws.com/ecr-test:latest
 echo "--------------- 서버 배포 끝 -----------------"
